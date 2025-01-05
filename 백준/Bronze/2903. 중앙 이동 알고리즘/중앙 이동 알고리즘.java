180c991fbf334fffdfdd9ebb910a02f7479fedb8
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-01-05 20:47:47 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 172 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int dot = 2;
+
+        for(int i=0; i<n; i++) {
+            dot = (dot+(dot-1));
+        }
+
+        System.out.println(dot*dot);
+
+        sc.close();
+    }
+}
