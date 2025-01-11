80cce69717c8ee0775dd4e951f8dfcde8ed612dd
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-01-11 19:02:29 +0900

    [Silver V] Title: 단어 정렬, Time: 436 ms, Memory: 22452 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -19,12 +19,15 @@ public class Main {
         Set<String> unique = new TreeSet<>(new Comparator<String>() {
             public int compare(String s1, String s2) {
                 if(s1.length() == s2.length()) {
+                    //알파벳순 정렬
                     return s1.compareTo(s2);
                 }
+                //길이순정렬
                 return s1.length() - s2.length();
             }
         });
 
+        //set을 이용해 중복된 단어를 자동제거, 정렬된 상태로 추가
         for(String word : arr) {
             unique.add(word);
         }
