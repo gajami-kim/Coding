237ff36149ec666b59449a4acc861452939495bb
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-02-18 22:30:44 +0900

    [level 0] Title: 짝수는 싫어요, Time: 4.11 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,19 +1,9 @@
-import java.util.*;
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int n) {
-         ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i=1; i<=n; i++) {
-            if(i%2==1) {
-                list.add(i);
-            }
-        }
-        
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+         return IntStream.rangeClosed(1, n)
+                .filter(i -> i % 2 == 1)
+                .toArray();
     }
 }
