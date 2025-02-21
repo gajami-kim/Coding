51ba87de461b31f3bcbad5ecaac0ecf3c20df2a3
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-02-21 19:13:06 +0900

    [level 0] Title: 배열 만들기 4, Time: 35.22 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,17 +1,14 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr) {
-        ArrayList<Integer> stk = new ArrayList<>();
+        Stack<Integer> stk = new Stack();
         int i=0;
         
-        while(i<arr.length) {
-            if(stk.isEmpty() || stk.get(stk.size()-1) < arr[i]) {
-                stk.add(arr[i]);
-            } else {
-                stk.remove(stk.size()-1);
-                continue; //원소 제거 후 i값 증가없이 다시 처리
-            }
-            i++;
+        while(i < arr.length) {
+            if(stk.isEmpty() || stk.peek() < arr[i]){
+                stk.push(arr[i]);
+                i++;  
+            } else { stk.pop(); }
         }
         
         
