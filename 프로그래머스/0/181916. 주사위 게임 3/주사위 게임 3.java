2ccd90660ea7d0152d7fa12c909493ddcd4446e4
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-02-26 23:18:17 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.13 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int a, int b, int c, int d) {
+        int[] dice = {a, b, c, d};
+        Arrays.sort(dice);
+        
+        if (dice[0] == dice[3]) return 1111 * dice[0];
+        if (dice[0] == dice[2] || dice[1] == dice[3]) return (int)Math.pow(dice[1] * 10 + (dice[0] + dice[3] - dice[1]),2);
+        if (dice[0] == dice[1] && dice[2] == dice[3]) return (dice[0] + dice[2]) * Math.abs(dice[0] - dice[2]);
+        if (dice[0] == dice[1]) return dice[2] * dice[3];
+        if (dice[1] == dice[2]) return dice[0] * dice[3];
+        if (dice[2] == dice[3]) return dice[0] * dice[1];
+        return dice[0];
+    }
+}
