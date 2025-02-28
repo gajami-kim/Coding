1ae213be9f8596a542c9da70e75cf36bc1269aaa
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-02-28 22:55:38 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,5 +1,14 @@
 class Solution {
     public int solution(int hp) {
-        return hp / 5 + (hp % 5) / 3 + (hp % 5) % 3;
+        int answer = 0;
+        
+        answer = hp / 5;
+        hp = hp % 5;
+        answer += hp / 3;
+        hp = hp % 3;
+        answer += hp / 1;
+        hp = hp % 1;
+        
+        return answer;
     }
 }
