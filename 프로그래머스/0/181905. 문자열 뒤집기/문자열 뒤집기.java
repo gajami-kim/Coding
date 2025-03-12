465b83998f027f84ecb47ce8da1253b8078a2ff3
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-03-12 17:40:26 +0900

    [level 0] Title: 문자열 뒤집기, Time: 18.79 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+        for(int i=s; i<=e; i++) {
+            answer = new StringBuilder(my_string.substring(s,e+1)).reverse().toString();
+        }
+        if(my_string.length()>e) {
+            return my_string.substring(0,s)+answer.toString()+my_string.substring(e+1, my_string.length());
+        }
+        return my_string.substring(0,s)+answer.toString();
+    }
+}
