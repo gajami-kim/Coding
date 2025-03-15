0dbd00e99a37372f0f5e88e630c4b5776606a82b
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-03-15 18:14:07 +0900

    [level 0] Title: 369게임, Time: 0.05 ms, Memory: 90.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String str = String.valueOf(order);
+        
+        for(char ch : str.toCharArray()) {
+            if(ch == '3' || ch == '6' || ch == '9') answer++;
+        }
+        
+        return answer;
+    }
+}
