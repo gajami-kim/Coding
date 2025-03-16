96600c12fd193e632f4976719db4698d7d710b9b
jungon <165994643+gajami-kim@users.noreply.github.com>
2025-03-16 21:36:47 +0900

    [level 0] Title: 영어가 싫어요, Time: 1.23 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -1,32 +1,12 @@
 import java.util.*;
 class Solution {
     public long solution(String numbers) {
-        Map<String, String> numberMap = new HashMap<>();
-        numberMap.put("one", "1");
-        numberMap.put("two", "2");
-        numberMap.put("three", "3");
-        numberMap.put("four", "4");
-        numberMap.put("five", "5");
-        numberMap.put("six", "6");
-        numberMap.put("seven", "7");
-        numberMap.put("eight", "8");
-        numberMap.put("nine", "9");
-        numberMap.put("zero", "0");
-
-        StringBuilder str = new StringBuilder();
-        int i = 0;
+        String[] number = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        while(i < numbers.length()) {
-            for(int j=3; j<=5; j++) {
-                String sub = numbers.substring(i, Math.min(i + j, numbers.length()));
-                if(numberMap.containsKey(sub)) {
-                    str.append(numberMap.get(sub));
-                    i += j;
-                    break;
-                }
-            }
+        for(int i=0; i<number.length; i++) {
+            numbers = numbers.replaceAll(number[i], String.valueOf(i));
         }
 
-        return Long.parseLong(str.toString());
+        return Long.parseLong(numbers);
     }
 }
